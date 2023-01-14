898ebbf894ba0d48ab52dcc8a26f152a38ee4e23
samuelahmed <samueladuval@gmail.com>
2023-01-14 03:48:33 -0800

    Slight adjustments to main layout responsiveness

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,8 @@ type LayoutProps = {
 const AuthLayout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen">
-      <main className="w-12/12 mx-auto flex flex-col p-2 md:w-8/12 md:p-4 xl:w-10/12">
+      {/* Managing high-level responsiveness */}
+      <main className="w-12/12 mx-auto flex flex-col p-2 md:w-10/12 md:p-4 xl:w-11/12">
         {children}
       </main>
     </div>
