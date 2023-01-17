9a8ae56c74b59f3e205b10836a56ebb2d15b5d77
samuelahmed <samueladuval@gmail.com>
2023-01-17 15:43:50 -0800

    Added hsl(0,0%,96%) as bg color

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ type LayoutProps = {
 
 const AuthLayout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen dark:bg-slate-800">
+    <div className="min-h-screen dark:bg-slate-800 bg-[hsl(0,0%,96%)]">
       {/* Managing high-level responsiveness */}
       <main className="w-12/12 mx-auto flex flex-col p-2 md:w-10/12 md:p-4 xl:w-11/12">
         {children}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: 'class',
   theme: {
+    // colors: {
+    //   bgLight: "#151819",
+    // },
     extend: {
       height: {
         '128': '32rem',
