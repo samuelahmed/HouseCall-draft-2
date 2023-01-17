4d3b2bc9168beb4cb4292efdea9f101c7f52b436
samuelahmed <samueladuval@gmail.com>
2023-01-17 14:11:30 -0800

    Removed purple gradient and added dark mode to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,19 +14,19 @@ const Home: NextPage = () => {
       </Head>
       <NavLayout />
       <Layout>
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <main className="flex min-h-screen flex-col items-center justify-center">
           <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            <h1 className="text-5xl font-extrabold tracking-tight text-black dark:text-white sm:text-[5rem]">
               House <span className="text-[hsl(280,100%,70%)]">Call</span>
             </h1>
             <div className="flex flex-col items-center gap-2">
-              <p className="text-3xl text-white">
+              <p className="text-3xl text-black dark:text-white">
                 Care in the comfort of your home
               </p>
             </div>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
               <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                className="flex max-w-xs flex-col gap-4 rounded-xl bg-black/10 p-4 text-black hover:bg-black/20 dark:bg-white/10 dark:text-white dark:hover:bg-white/20"
                 href="/caregiver"
               >
                 <h3 className="text-2xl font-bold">Caregivers →</h3>
@@ -35,7 +35,7 @@ const Home: NextPage = () => {
                 </div>
               </Link>
               <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                className="flex max-w-xs flex-col gap-4 rounded-xl bg-black/10 p-4 text-black hover:bg-black/20 dark:bg-white/10 dark:text-white dark:hover:bg-white/20"
                 href="/patient"
               >
                 <h3 className="text-2xl font-bold">Patients →</h3>
