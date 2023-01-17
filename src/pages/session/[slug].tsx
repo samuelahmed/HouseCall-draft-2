cba7f152ed50930cee990b65766a723fb7155a00
samuelahmed <samueladuval@gmail.com>
2023-01-17 03:01:34 -0800

    Fix styling

--- src/pages/session/[slug].tsx
+++ src/pages/session/[slug].tsx
@@ -8,8 +8,8 @@ const Slug: NextPage = () => {
   const router = useRouter();
   const { slug } = router.query;
   if (typeof slug !== "string") return null;
-
   const { data: card } = trpc.sessionAPIs.getOneSession.useQuery({ slug });
+  
   return (
     <>
       <Head>
@@ -19,7 +19,7 @@ const Slug: NextPage = () => {
       <div className="flex h-screen items-center justify-center dark:bg-gray-800">
         <div className="mx-2 my-2 h-4/6 w-1/2 rounded-lg border-2 dark:bg-sky-900">
           <div className="mb-4 mr-4 ml-4">
-            <div className="mb-2 mt-12 p-4 text-center  text-xl text-gray-800 dark:text-white">
+            <div className="mb-2 mr-4 ml-4 mt-12 p-4 text-center  text-xl text-gray-900 dark:text-white">
               {card?.title}
             </div>
             <div className="text-sm">
@@ -50,7 +50,7 @@ const Slug: NextPage = () => {
             </div>
           </div>
           <div className="mt-12 mb-12 flex justify-around ">
-            <button className="h-12 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-700 hover:border-gray-700 hover:bg-emerald-200 hover:text-black dark:text-white">
+            <button className="h-12 rounded border border-gray-500 bg-transparent px-4 pt-2 pb-8 font-semibold text-gray-900 hover:border-gray-700 hover:bg-emerald-200 hover:text-black dark:text-white">
               Schedule Session
             </button>
           </div>
