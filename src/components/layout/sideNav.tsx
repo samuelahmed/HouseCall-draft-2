96f56916ef564e6f91eaa19ca86ffc1618a20514
samuelahmed <samueladuval@gmail.com>
2023-01-17 19:25:40 -0800

    Fix lightmode bg color of navmenu

--- src/components/layout/sideNav.tsx
+++ src/components/layout/sideNav.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const SideNav = () => {
   return (
     <div className="ml-0.5 fixed z-30 w-64 shadow-sm">
-      <div className="fixed z-50 grid grid-rows-1 bg-gray-100 dark:bg-gray-700">
+      <div className="fixed z-50 grid grid-rows-1 bg-[hsl(0,0%,88%)] dark:bg-gray-700">
         <Link href={"/caregiver"} className=" border py-1 px-4  border-gray-200 dark:border-gray-800">
           Caregiver Dashboard
         </Link>
