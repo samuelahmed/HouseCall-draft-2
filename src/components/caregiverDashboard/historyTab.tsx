d8d3289c8ede960bb9167e1bc76280df5fd183f9
samuelahmed <samueladuval@gmail.com>
2023-01-17 17:30:43 -0800

    Fix styling history tab

--- src/components/caregiverDashboard/historyTab.tsx
+++ src/components/caregiverDashboard/historyTab.tsx
@@ -5,11 +5,11 @@ const HistoryTab = () => {
   return (
     <>
       {/* MAIN SECTION */}
-      <div className="grid grid-rows-1 rounded-b bg-gray-100 px-4 dark:bg-gray-700">
+      <div className="grid grid-rows-1 rounded-b bg-[hsl(0,0%,88%)] px-4 dark:bg-gray-700">
         <div className="grid grid-cols-1 pt-2 pb-2 md:grid-cols-1">
           {/* Job Session Cards */}
           <div className="h-full overflow-scroll pr-2 md:max-h-screen lg:max-h-screen ">
-            <div className="grid justify-items-center gap-4 rounded bg-gray-200 pt-6 pb-6 dark:bg-gray-700">
+            <div className="grid justify-items-center gap-4 rounded bg-[hsl(0,0%,88%)] pt-6 pb-6 dark:bg-gray-700">
               <div className="flex w-11/12 cursor-pointer flex-col  justify-between  rounded-xl border border-gray-400 bg-white p-2 leading-normal hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-600">
                 <div className="items-baseline md:flex md:justify-around ">
                   <HistoryOverviewCard />
@@ -98,7 +98,6 @@ const HistoryTab = () => {
                   </div>
                 </div>
               </div>
-          
             </div>
           </div>
         </div>
