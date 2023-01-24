031579f9cfb7309df5d55dc895592be5a05f1302
samuelahmed <samueladuval@gmail.com>
2023-01-24 12:19:20 -0800

    Removed the responsive layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import ResponsiveLayout from "@/components/layout/responsiveLayout";
 import NavLayout from "@/components/layout/navLayout";
 
 const Home: NextPage = () => {
@@ -13,40 +12,38 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavLayout />
-      <ResponsiveLayout>
-        <main className="flex min-h-screen flex-col items-center justify-center">
-          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-            <h1 className="text-5xl font-extrabold tracking-tight text-gray-800 dark:text-gray-100 sm:text-[5rem]">
-              House <span className="text-[hsl(280,100%,70%)]">Call</span>
-            </h1>
-            <div className="flex flex-col items-center gap-2">
-              <p className="text-3xl text-gray-800 dark:text-gray-100">
-                Care in the comfort of your home
-              </p>
-            </div>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-black/10 p-4 text-gray-800 hover:bg-black/20 dark:bg-white/10 dark:text-gray-100 dark:hover:bg-white/20"
-                href="/caregiver"
-              >
-                <h3 className="text-2xl font-bold">Caregivers →</h3>
-                <div className="text-lg">
-                  You are passionate to help those in need.
-                </div>
-              </Link>
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-black/10 p-4 text-gray-800 hover:bg-black/20 dark:bg-white/10 dark:text-gray-100 dark:hover:bg-white/20"
-                href="/patient"
-              >
-                <h3 className="text-2xl font-bold">Patients →</h3>
-                <div className="text-lg">
-                  You or someone you care about needs assistance.
-                </div>
-              </Link>
-            </div>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-gray-800 dark:text-gray-100 sm:text-[5rem]">
+            House <span className="text-[hsl(280,100%,70%)]">Call</span>
+          </h1>
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-3xl text-gray-800 dark:text-gray-100">
+              Care in the comfort of your home
+            </p>
           </div>
-        </main>
-      </ResponsiveLayout>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-black/10 p-4 text-gray-800 hover:bg-black/20 dark:bg-white/10 dark:text-gray-100 dark:hover:bg-white/20"
+              href="/caregiver"
+            >
+              <h3 className="text-2xl font-bold">Caregivers →</h3>
+              <div className="text-lg">
+                You are passionate to help those in need.
+              </div>
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-black/10 p-4 text-gray-800 hover:bg-black/20 dark:bg-white/10 dark:text-gray-100 dark:hover:bg-white/20"
+              href="/patient"
+            >
+              <h3 className="text-2xl font-bold">Patients →</h3>
+              <div className="text-lg">
+                You or someone you care about needs assistance.
+              </div>
+            </Link>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
--- src/pages/loginSuccess.tsx
+++ src/pages/loginSuccess.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { trpc } from "@/utils/trpc";
-import ResponsiveLayout from "@/components/layout/responsiveLayout";
 import NavLayout from "@/components/layout/navLayout";
 
 const LoginSuccess: NextPage = () => {
@@ -18,29 +17,27 @@ const LoginSuccess: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavLayout />
-      <ResponsiveLayout>
-        {session && (
-          <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b">
-            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-              <h1 className="text-5xl font-extrabold tracking-tight text-black dark:text-white sm:text-[5rem]">
-                Hello{" "}
-                <span className="text-[hsl(280,100%,70%)]">
-                  {isLoading || (
-                      <span className="text-[hsl(280,100%,70%)]">
-                        {data && data?.username}
-                      </span>
-                    ) || <span className="text-red-600">Meow! No Name</span>}
-                </span>
-              </h1>
-            </div>
-          </main>
-        )}
-        {!session && (
-          <main className="flex min-h-screen flex-col items-center justify-top md:justify-center lg:justify-center">
+      {session && (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b">
           <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-            <h1 className="text-5xl font-extrabold tracking-tight text-gray-800 dark:text-white sm:text-[5rem] text-center border-gray-900">
-              House{" "}
-              <span className="text-[hsl(280,100%,70%)]">Call</span>
+            <h1 className="text-5xl font-extrabold tracking-tight text-black dark:text-white sm:text-[5rem]">
+              Hello{" "}
+              <span className="text-[hsl(280,100%,70%)]">
+                {isLoading || (
+                    <span className="text-[hsl(280,100%,70%)]">
+                      {data && data?.username}
+                    </span>
+                  ) || <span className="text-red-600">Meow! No Name</span>}
+              </span>
+            </h1>
+          </div>
+        </main>
+      )}
+      {!session && (
+        <main className="justify-top flex min-h-screen flex-col items-center md:justify-center lg:justify-center">
+          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+            <h1 className="border-gray-900 text-center text-5xl font-extrabold tracking-tight text-gray-800 dark:text-white sm:text-[5rem]">
+              House <span className="text-[hsl(280,100%,70%)]">Call</span>
             </h1>
             <div className="flex flex-row gap-2">
               <Link href={"/signin"} className="rounded border py-1 px-4">
@@ -52,8 +49,7 @@ const LoginSuccess: NextPage = () => {
             </div>
           </div>
         </main>
-        )}
-      </ResponsiveLayout>
+      )}
     </>
   );
 };
