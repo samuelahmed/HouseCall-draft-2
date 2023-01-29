bc1352aab4f05c96da0c2cf630675dc5111bf740
samuelahmed <samueladuval@gmail.com>
2023-01-28 16:04:05 -0800

    Deleted unsued example router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,12 +1,10 @@
 import { router } from "../trpc";
 import { protectedRouter } from "./protected";
-// import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
 import { sessionRouter } from "./sessionRouter";
 import { userRouter } from "./userRouter";
 
 export const appRouter = router({
-  // example: exampleRouter,
   protected: protectedRouter,
   auth: authRouter,
   sessionAPIs: sessionRouter,
