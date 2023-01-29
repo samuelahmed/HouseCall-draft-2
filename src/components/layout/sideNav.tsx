3d7b886dcd9729a8a37b97490131aae93d3e936f
samuelahmed <samueladuval@gmail.com>
2023-01-28 16:32:07 -0800

    Update menu to only display if session & to display by User Role

--- src/components/layout/sideNav.tsx
+++ src/components/layout/sideNav.tsx
@@ -1,31 +1,120 @@
-// ISSUE 1: MENU BREAKS AFTER SCROLL ON REAL IPHONE TEST. NOT REPLICATED ON BROWSER PHONE MODE. RESOLVE ASAP
 import Link from "next/link";
+import { trpc } from "@/utils/trpc";
+import { useSession } from "next-auth/react";
 
 const SideNav = () => {
+  const { data, isLoading } = trpc.userAPIs.readCurrentUser.useQuery();
+  const { data: session } = useSession();
   return (
-    <div className="ml-0.5 fixed z-30 w-64 shadow-sm">
-      <div className="fixed z-50 grid grid-rows-1 bg-[hsl(0,0%,88%)] dark:bg-gray-700 text-lg md:text-lg xl:text-xl">
-        <Link href={"/caregiver"} className=" border py-1 px-4  border-gray-200 dark:border-gray-800">
-          Caregiver Dashboard
-        </Link>
-        <Link href={"/patient"} className=" border py-1 px-4  border-gray-200 dark:border-gray-800">
-          Patient Dashboard
-        </Link>
-        <Link href={"/messages"} className=" border py-1 px-4  border-gray-200 dark:border-gray-800">
-          Messages
-        </Link>
-        <Link href={"/account"} className=" border py-1 px-4  border-gray-200 dark:border-gray-800">
-          Account
-        </Link>
-        <Link href={"/help"} className=" border py-1 px-4  border-gray-200 dark:border-gray-800">
-          Help
-        </Link>
-        {/* <Link href={"/test"} className="rounded border py-1 px-4">
-        Test
-      </Link> */}
-      </div>
-    </div>
+    <>
+      {(session && data?.role === 'Caregiver') && (
+        <>
+          <div className="fixed z-30 ml-0.5 w-64 shadow-sm">
+            <div className="fixed z-50 grid grid-rows-1 bg-[hsl(0,0%,88%)] text-lg dark:bg-gray-700 md:text-lg xl:text-xl">
+              <Link
+                href={"/caregiver"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Caregiver Dashboard
+              </Link>
+              <Link
+                href={"/messages"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Messages
+              </Link>
+              <Link
+                href={"/account"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Account
+              </Link>
+              <Link
+                href={"/help"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Help
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
+      {(session && data?.role === 'Patient') && (
+        <>
+          <div className="fixed z-30 ml-0.5 w-64 shadow-sm">
+            <div className="fixed z-50 grid grid-rows-1 bg-[hsl(0,0%,88%)] text-lg dark:bg-gray-700 md:text-lg xl:text-xl">
+              <Link
+                href={"/patient"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Patient Dashboard
+              </Link>
+              <Link
+                href={"/messages"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Messages
+              </Link>
+              <Link
+                href={"/account"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Account
+              </Link>
+              <Link
+                href={"/help"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Help
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
+      {(session && data?.role === 'Caregiver & Patient') && (
+        <>
+          <div className="fixed z-30 ml-0.5 w-64 shadow-sm">
+            <div className="fixed z-50 grid grid-rows-1 bg-[hsl(0,0%,88%)] text-lg dark:bg-gray-700 md:text-lg xl:text-xl">
+              <Link
+                href={"/caregiver"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Caregiver Dashboard
+              </Link>
+              <Link
+                href={"/patient"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Patient Dashboard
+              </Link>
+              <Link
+                href={"/messages"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Messages
+              </Link>
+              <Link
+                href={"/account"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Account
+              </Link>
+              <Link
+                href={"/help"}
+                className=" border border-gray-200 py-1  px-4 dark:border-gray-800"
+              >
+                Help
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
+      {!session && <>
+      </>}
+    </>
   );
 };
 
 export default SideNav;
+
+
