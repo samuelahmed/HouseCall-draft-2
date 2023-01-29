f3e484aea033b9573e2d163f780e53da317b7c97
samuelahmed <samueladuval@gmail.com>
2023-01-28 16:05:45 -0800

    reactive query on protected router

--- src/server/trpc/router/protected.ts
+++ src/server/trpc/router/protected.ts
@@ -1,9 +1,10 @@
 import { router, publicProcedure, privateProcedure } from "../trpc";
 
+//make sure this is working as intended
 export const protectedRouter = router({
-  // getSession: publicProcedure.query(({ ctx }) => {
-  //   return ctx.session;
-  // }),
+  getSession: publicProcedure.query(({ ctx }) => {
+    return ctx.session;
+  }),
   getSecretMessage: privateProcedure.query(() => {
     return "you can now see this secret message!";
   }),
