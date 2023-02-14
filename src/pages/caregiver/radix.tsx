e62adfb137bc6ca97e2ac874ac5fff9825d326fe
samuelahmed <samueladuval@gmail.com>
2023-02-14 11:11:04 -0800

    Outside for find logic - still unstyled or using old style

--- src/pages/caregiver/radix.tsx
+++ src/pages/caregiver/radix.tsx
@@ -16,12 +16,43 @@ import * as Tabs from "@radix-ui/react-tabs";
 import * as Switch from "@radix-ui/react-switch";
 import * as Separator from "@radix-ui/react-separator";
 
-import { ChatBubbleIcon, FaceIcon, PersonIcon, CalendarIcon, IdCardIcon, ArchiveIcon, QuestionMarkIcon } from "@radix-ui/react-icons";
+import {
+  ChatBubbleIcon,
+  FaceIcon,
+  PersonIcon,
+  CalendarIcon,
+  IdCardIcon,
+  ArchiveIcon,
+  QuestionMarkIcon,
+} from "@radix-ui/react-icons";
+import { useRouter } from "next/router";
+import { trpc } from "@/utils/trpc";
 
 const Caregiver: NextPage = (props) => {
   const { data: session } = useSession();
   const [openSide, setOpenSide] = useState(0);
   const [openTab, setOpenTab] = useState(1);
+  const [rightCard, setRightCard] = useState(1);
+  const { data, isLoading } = trpc.careSessionAPIs.readAllSessions.useQuery();
+  const router = useRouter();
+
+  const [inputs, setInputs] = useState({
+    title: "",
+    id: "",
+    name: "",
+    address: "",
+    overview: "",
+    sessionType: "",
+    hourlyRate: 0,
+    totalHours: 0,
+    totalCompensation: 0,
+    careSessionStatus: "",
+  });
+
+  const selectedSession =
+    trpc.careSessionAPIs.readOneSessionBySessionId.useQuery({
+      id: inputs?.id || (data?.[data?.length - 1]?.id ?? "0"),
+    });
 
   return (
     <>
@@ -69,7 +100,7 @@ const Caregiver: NextPage = (props) => {
                     <CalendarIcon className="mr-2" />
                     Scheduled
                   </Link>
-                  
+
                   <Link href={"/help"} className="flex flex-row items-center">
                     <ArchiveIcon className="mr-2" />
                     History
@@ -108,14 +139,108 @@ const Caregiver: NextPage = (props) => {
                   <div
                     className="
                     col-span-2
-                    border border-blue6
-                    bg-blue2
-                    dark:border-darkBlue6 dark:bg-darkBlue1
+                    max-h-90vh overflow-scroll
+                    border
+                    border-blue6 bg-blue2
+                    dark:border-darkBlue6
+                    dark:bg-darkBlue1
                     md:col-span-1
+                  
                   "
                   >
-                    Left Table
+                    {/* Left Table */}
+                    <div
+                      className="mt-4"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setRightCard(1);
+                      }}
+                    >
+                      <ul>
+                        {data
+                          ?.map((data) => {
+                            const {
+                              id,
+                              title,
+                              name,
+                              address,
+                              overview,
+                              sessionType,
+                              hourlyRate,
+                              totalHours,
+                              totalCompensation,
+                              careSessionStatus,
+                            } = data;
+                            return (
+                              <li
+                                key={id}
+                                // this className makes the cards
+                                className="boder-gray-400 bg-white hover:bg-gray-100 dark:border-gray-400 dark:bg-gray-800 dark:hover:bg-gray-600 mx-2 mb-2 cursor-pointer items-center justify-around rounded-lg border px-2"
+                              >
+                                <div
+                                  onClick={() => {
+                                    setInputs({
+                                      id: id,
+                                      title: title || "still loading",
+                                      name: name || "still loading",
+                                      address: address || "still loading",
+                                      overview: overview || "still loading",
+                                      sessionType:
+                                        sessionType || "still loading",
+                                      careSessionStatus:
+                                        careSessionStatus || "still loading",
+                                      hourlyRate: Number(data.hourlyRate) || 0,
+                                      totalHours: Number(data.totalHours) || 0,
+                                      totalCompensation:
+                                        Number(data.totalCompensation) ||
+                                        (Number(totalHours) || 0) *
+                                          (Number(hourlyRate) || 0),
+                                    });
+                                  }}
+                                  className="mb-8"
+                                >
+                                  <div className="">{title}</div>
+                                  <p className="">
+                                    <span className="">Status:&nbsp;</span>
+                                    {careSessionStatus}
+                                  </p>
+                                  <p className="">
+                                    <span className="">Name:&nbsp;</span>
+                                    {name}
+                                  </p>
+                                  <p className="">
+                                    <span className="">Address:&nbsp;</span>
+                                    {address}
+                                  </p>
+                                  <p className="">
+                                    <span className="">Overview:&nbsp;</span>
+                                    {overview}
+                                  </p>
+                                  <p className="">
+                                    <span className="">
+                                      Total Compensation:&nbsp;
+                                    </span>
+                                    ${totalCompensation}
+                                  </p>
+                                </div>
+                                <div className="mb-4 flex flex-col items-center justify-center">
+                                  <button
+                                    onClick={() =>
+                                      router.push(`/careSession/${data.slug}`)
+                                    }
+                                    className="border-gray-500 bg-transparent text-gray-700 hover:border-gray-700 hover:bg-emerald-200 hover:text-black dark:text-white h-10 rounded border px-4 pt-2 pb-8 font-semibold md:hidden lg:hidden"
+                                  >
+                                    Schedule Session
+                                  </button>
+                                </div>
+                              </li>
+                            );
+                          })
+                          .reverse()}
+                      </ul>
+                    </div>
                   </div>
+
                   <div
                     className="
                     hidden border
@@ -125,7 +250,82 @@ const Caregiver: NextPage = (props) => {
                     md:block
                   "
                   >
-                    Right Table
+                    {/* Right Table */}
+                    {/* this className is for the right card */}
+                    <div className="border-gray-400 bg-white dark:bg-gray-800 flex  min-w-max flex-col justify-between rounded-xl border mt-4 mx-2 min-h-85vh">
+                      <div className={rightCard === 1 ? "" : "hidden"}>
+                        <div className="mb-4 mr-4 ml-4 justify-center ">
+                          <div className="text-gray-800 dark:text-gray-100 mb-2 p-4 text-center text-xl">
+                            {selectedSession?.data?.title || isLoading}
+                          </div>
+                          <div className="text-sm">
+                            <p className="text-gray-900 dark:text-gray-100">
+                              <span className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Status:&nbsp;
+                              </span>
+                              {selectedSession?.data?.careSessionStatus ||
+                                isLoading}
+                            </p>
+                            <p className="text-gray-900 dark:text-gray-100">
+                              <span className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Name:&nbsp;
+                              </span>
+                              {selectedSession?.data?.name || isLoading}
+                            </p>
+                            <p className="text-gray-900 dark:text-gray-100">
+                              <span className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Address:&nbsp;
+                              </span>
+                              {selectedSession?.data?.address || isLoading}
+                            </p>
+                            <p className="text-gray-900 dark:text-gray-100">
+                              <span className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Medical Notes:&nbsp;
+                              </span>
+                              {selectedSession?.data?.medicalNotes || isLoading}
+                            </p>
+                            <p className="text-gray-900 dark:text-gray-100">
+                              <span className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Overview:&nbsp;
+                              </span>
+                              {selectedSession?.data?.overview || isLoading}
+                            </p>
+                            <p className="text-gray-900 dark:text-gray-100">
+                              <span className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Hourly Rate:&nbsp;
+                              </span>
+                              ${selectedSession?.data?.hourlyRate || isLoading}
+                            </p>
+                            <p className="text-gray-900 dark:text-gray-100">
+                              <span className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Hours:&nbsp;
+                              </span>
+                              {selectedSession?.data?.totalHours || isLoading}
+                            </p>
+                            <p className="text-gray-900 dark:text-gray-100">
+                              <span className="text-gray-800 dark:text-gray-200 font-semibold">
+                                Total:&nbsp;
+                              </span>
+                              $
+                              {selectedSession?.data?.totalCompensation ||
+                                isLoading}
+                            </p>
+                            <div className="flex flex-col items-center justify-center">
+                              <button
+                                onClick={() =>
+                                  router.push(
+                                    `/careSession/${selectedSession.data?.slug}`
+                                  )
+                                }
+                                className="hover:border-hsl(0,0%,6%) hover:text-hsl(0,0%,6%) border-gray-500 bg-transparent text-gray-800 dark:text-gray-100 dark:hover:text-gray-800 mt-6 h-10 rounded border px-4 pt-2 pb-8 font-semibold hover:bg-[hsl(154,47%,66%)]"
+                              >
+                                Schedule
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
