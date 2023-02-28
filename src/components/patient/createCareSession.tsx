8ebe8d65e7359a0c2171884178e1263081ba9fd4
samuelahmed <samueladuval@gmail.com>
2023-02-28 15:54:28 -0800

    Make pricing automatically count the next hours and rount to ceil

--- src/components/patient/createCareSession.tsx
+++ src/components/patient/createCareSession.tsx
@@ -57,20 +57,24 @@ const CreateSession = () => {
     overview: "",
     title: "",
     hourlyRate: 20,
-    totalHours: endTime.hour - startTime.hour,
+    totalHours: Math.ceil(endTime.hour - startTime.hour),
     totalCompensation: 20,
     acceptedCaregiverId: "",
     careSessionStatus: "",
   });
 
   useEffect(() => {
-    const hours = endTime.hour - startTime.hour;
-    const minutes = endTime.minute - startTime.minute;
-    const seconds = endTime.second - startTime.second;
-
-    const totalHours = hours + minutes / 60 + seconds / 3600;
+    // const hours = endTime && startTime ? endTime.hour - startTime.hour : 0;
+    // const minutes =
+    //   endTime && startTime ? endTime.minute - startTime.minute : 0;
+    // const seconds =
+    //   endTime && startTime ? endTime.second - startTime.second : 0;
+    const totalHours = Math.ceil(
+      endTime.hour +
+        endTime.minute / 60 -
+        (startTime.hour + startTime.minute / 60)
+    );
     const totalCompensation = totalHours * inputs.hourlyRate;
-
     setInputs((prevInputs) => ({
       ...prevInputs,
       totalHours,
@@ -78,7 +82,7 @@ const CreateSession = () => {
     }));
   }, [startTime, endTime, inputs.hourlyRate]);
 
-  const totalComp = inputs.totalHours * inputs.hourlyRate;
+  const totalComp = Math.ceil(inputs.totalHours * inputs.hourlyRate);
 
   useEffect(() => {
     setInputs((prev) => {
@@ -116,11 +120,9 @@ const CreateSession = () => {
   // console.log(`Hour: ${startTime.hour}`);
   // console.log(`Minute: ${startTime.minute}`);
   // console.log(`Second: ${startTime.second}`);
-
   // console.log(`Hour: ${endTime.hour}`);
   // console.log(`Minute: ${endTime.minute}`);
   // console.log(`Second: ${endTime.second}`);
-
   // const total = endTime.hour - startTime.hour;
   // console.log("total Hours" + total);
   // console.log(inputs.totalHours);
@@ -213,26 +215,15 @@ const CreateSession = () => {
               </div>
             </div>
 
-          <div className="col-span-1 grid min-h-88vh grid-rows-4 space-y-2">
-            <div className="row-span-2 border border-blue7 bg-blue1 dark:border-darkBlue7 dark:bg-darkBlue1">
-              <h1 className="text-center text-lg font-extralight">Location</h1>
+            <div className="col-span-1 grid min-h-88vh grid-rows-4 space-y-2">
+              <div className="row-span-2 border border-blue7 bg-blue1 dark:border-darkBlue7 dark:bg-darkBlue1">
+                <h1 className="text-center text-lg font-extralight">
+                  Location
+                </h1>
 
-              <div className="  mx-4 mb-2 flex min-w-full flex-col pr-8 text-sm">
-                <Label.Root className="px-0.5" htmlFor="firstName">
-                  Address
-                </Label.Root>
-                <input
-                  className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
-                  type="text"
-                  id="firstName"
-                  defaultValue=""
-                />
-              </div>
-
-              <div className="flex-col-1 flex max-w-fit text-sm">
-                <div className="col-span-1 mx-4 flex max-w-fit flex-col text-sm">
+                <div className="  mx-4 mb-2 flex min-w-full flex-col pr-8 text-sm">
                   <Label.Root className="px-0.5" htmlFor="firstName">
-                    City
+                    Address
                   </Label.Root>
                   <input
                     className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
@@ -241,106 +232,118 @@ const CreateSession = () => {
                     defaultValue=""
                   />
                 </div>
-                <div className="col-span-1 mx-4 flex max-w-fit flex-col text-sm">
-                  <Label.Root className="px-0.5" htmlFor="firstName">
-                    Postal Code
-                  </Label.Root>
-                  <input
-                    className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
-                    type="text"
-                    id="firstName"
-                    defaultValue=""
-                  />
-                </div>
-              </div>
 
-              <div className="mx-4 mb-2 flex w-full flex-col  pt-2 pr-6 text-sm ">
-                <Label.Root className="px-0.5" htmlFor="firstName">
-                  Describe Location
-                </Label.Root>
-                <textarea
-                  className="inline-block h-32 w-full border
+                <div className="flex-col-1 flex max-w-fit text-sm">
+                  <div className="col-span-1 mx-4 flex max-w-fit flex-col text-sm">
+                    <Label.Root className="px-0.5" htmlFor="firstName">
+                      City
+                    </Label.Root>
+                    <input
+                      className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
+                      type="text"
+                      id="firstName"
+                      defaultValue=""
+                    />
+                  </div>
+                  <div className="col-span-1 mx-4 flex max-w-fit flex-col text-sm">
+                    <Label.Root className="px-0.5" htmlFor="firstName">
+                      Postal Code
+                    </Label.Root>
+                    <input
+                      className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
+                      type="text"
+                      id="firstName"
+                      defaultValue=""
+                    />
+                  </div>
+                </div>
+
+                <div className="mx-4 mb-2 flex w-full flex-col  pt-2 pr-6 text-sm ">
+                  <Label.Root className="px-0.5" htmlFor="firstName">
+                    Describe Location
+                  </Label.Root>
+                  <textarea
+                    className="inline-block h-32 w-full border
                     border-blue7 bg-blue1 px-1 py-1 align-text-top dark:border-darkBlue7 dark:bg-darkBlue1"
-                  // type="text"
-                  id="firstName"
-                  defaultValue=""
-                  value={inputs.medicalNotes}
-                  onChange={(e) =>
-                    setInputs((prev) => ({
-                      ...prev,
-                      medicalNotes: e.target.value,
-                    }))
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="row-span-2 border border-blue7 bg-blue1 dark:border-darkBlue7 dark:bg-darkBlue1">
-              <h1 className="text-center text-lg font-extralight">
-                Compensation
-              </h1>
-
-              <div className=" max-w-fit text-sm">
-                <div className="col-span-1 mx-4 flex max-w-fit flex-col text-sm">
-                  <Label.Root className="px-0.5" htmlFor="firstName">
-                    Hourly Rate
-                  </Label.Root>
-                  <input
-                    className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
+                    // type="text"
                     id="firstName"
-                    type="number"
-                    value={inputs.hourlyRate}
-                    onChange={(e) => {
+                    defaultValue=""
+                    value={inputs.medicalNotes}
+                    onChange={(e) =>
                       setInputs((prev) => ({
                         ...prev,
-                        hourlyRate: parseFloat(e.target.value),
-                        totalCompensation:
-                          parseFloat(e.target.value) * prev.totalHours,
-                      }));
-                    }}
+                        medicalNotes: e.target.value,
+                      }))
+                    }
                   />
                 </div>
-                <div className="col-span-1 mx-4 flex max-w-fit flex-col text-sm">
-                  <Label.Root className="px-0.5" htmlFor="firstName">
-                    Number of Hours
-                  </Label.Root>
-                  <input
-                    className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
-                    id="firstName"
-                    type="number"
-                    value={inputs.totalHours}
-                    // onChange={(e) => {
-                    //   setInputs((prev) => ({
-                    //     ...prev,
-                    //     totalHours: parseFloat(e.target.value),
-                    //     totalCompensation:
-                    //       prev.hourlyRate * parseFloat(e.target.value),
-                    //   }));
-                    // }}
-                  />
-                </div>
-              </div>
-              <div className="  mx-4 mb-2 flex min-w-full flex-col pr-8 text-sm">
-                <Label.Root className="px-0.5" htmlFor="firstName">
-                  Total: ${totalComp}
-                </Label.Root>
               </div>
 
-              <div className="flex items-center justify-center">
-                <button
-                  type="button"
-                  onClick={() => {
-                    publish();
-                  }}
-                  className="cursor-pointer border border-solid border-blue7 bg-blue3 px-3 text-base text-olive12 hover:border-blue8 hover:bg-blue4 
+              <div className="row-span-2 border border-blue7 bg-blue1 dark:border-darkBlue7 dark:bg-darkBlue1">
+                <h1 className="text-center text-lg font-extralight">
+                  Compensation
+                </h1>
+
+                <div className=" max-w-fit text-sm">
+                  <div className="col-span-1 mx-4 flex max-w-fit flex-col text-sm">
+                    <Label.Root className="px-0.5" htmlFor="firstName">
+                      Hourly Rate
+                    </Label.Root>
+                    <input
+                      className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
+                      id="firstName"
+                      type="number"
+                      value={inputs.hourlyRate}
+                      onChange={(e) => {
+                        setInputs((prev) => ({
+                          ...prev,
+                          hourlyRate: parseFloat(e.target.value),
+                          totalCompensation:
+                            parseFloat(e.target.value) * prev.totalHours,
+                        }));
+                      }}
+                    />
+                  </div>
+                  <div className="col-span-1 mx-4 flex max-w-fit flex-col text-sm">
+                    <Label.Root className="px-0.5" htmlFor="firstName">
+                      Number of Hours
+                    </Label.Root>
+                    <input
+                      className="border border-blue7 bg-blue1 px-1 py-1 dark:border-darkBlue7 dark:bg-darkBlue1"
+                      id="firstName"
+                      type="number"
+                      value={inputs.totalHours}
+                      // onChange={(e) => {
+                      //   setInputs((prev) => ({
+                      //     ...prev,
+                      //     totalHours: parseFloat(e.target.value),
+                      //     totalCompensation:
+                      //       prev.hourlyRate * parseFloat(e.target.value),
+                      //   }));
+                      // }}
+                    />
+                  </div>
+                </div>
+                <div className="  mx-4 mb-2 flex min-w-full flex-col pr-8 text-sm">
+                  <Label.Root className="px-0.5" htmlFor="firstName">
+                    Total: ${totalComp}
+                  </Label.Root>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      publish();
+                    }}
+                    className="cursor-pointer border border-solid border-blue7 bg-blue3 px-3 text-base text-olive12 hover:border-blue8 hover:bg-blue4 
             dark:border-darkBlue7 dark:bg-darkBlue3 dark:text-darkOlive12 dark:hover:border-darkBlue8 dark:hover:bg-darkBlue4"
-                >
-                  Create
-                </button>
+                  >
+                    Create
+                  </button>
+                </div>
               </div>
             </div>
-            </div>
-                  
           </div>
         </div>
         <div></div>
