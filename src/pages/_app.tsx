8793d4ba684ca3828358522dbdc05e71ab6f1754
samuelahmed <samueladuval@gmail.com>
2023-04-03 12:56:33 -0700

    test deploy _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { trpc } from "@/utils/trpc";
 import "../styles/globals.css";
-import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -15,19 +14,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <ThemeProvider attribute="class">
       <SessionProvider session={session}>
-        {/* Added this <Head> to force propagate favicon to all pages.
-        Make sure to review for better possible solution. */}
-        <Head>
-          <link rel="icon" href="/faviconLarge.png" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Slab&family=Roboto:ital@0;1&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
         <main className="font-robotoSlab">
-          {/* pass robotoSlab as main font - however for detailed text break it down to roboto only */}
           <Component {...pageProps} />
         </main>
       </SessionProvider>
