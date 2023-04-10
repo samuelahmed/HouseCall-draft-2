d80fa99589e35ae9dfaafebdfe8cbd9f5f1df58a
samuelahmed <samueladuval@gmail.com>
2023-04-10 15:24:53 -0700

    Add header and remove unused styling

--- src/pages/dashboard/caregiver/scheduled.tsx
+++ src/pages/dashboard/caregiver/scheduled.tsx
@@ -1,9 +1,9 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-// import NavLayout from "../../../components/layout/navLayout";
 import { useSession } from "next-auth/react";
 import LoginForm from "@/components/forms/loginForm";
 import ScheduledEngine from "@/components/caregiver/scheduledEngine";
+import Header from "@/components/layout/header";
 
 const Scheduled: NextPage = () => {
   const { data: session } = useSession();
@@ -13,20 +13,14 @@ const Scheduled: NextPage = () => {
       <Head>
         <title>Scheduled</title>
       </Head>
-      {/* <NavLayout /> */}
+      <Header />
       <div>
         {session && (
           <>
-            <main className="grid grid-cols-1 bg-blue1 dark:bg-darkBlue1 md:grid-cols-6">
-              <div className="col-span-5 min-w-fit bg-blue1 dark:bg-darkBlue1">
-                <div className="mx-4 mt-4 mb-1 flex items-center text-olive12 dark:text-darkOlive12">
-                </div>
-                <div className="mx-4 grid min-h-88vh grid-cols-2 gap-x-1 bg-blue1 dark:bg-darkBlue1">
-                  {/* DYNAMIC PART OF DASHBOARD */}
-                  <ScheduledEngine />
-                </div>
-              </div>
-            </main>
+            <div className="mx-4 grid min-h-88vh grid-cols-2 gap-x-1 bg-blue1 dark:bg-darkBlue1">
+              {/* DYNAMIC PART OF DASHBOARD */}
+              <ScheduledEngine />
+            </div>
           </>
         )}
         {!session && (
