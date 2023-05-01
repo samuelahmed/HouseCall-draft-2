f1d9299202511345758544b670844a244966bb5b
samuelahmed <samueladuval@gmail.com>
2023-05-01 16:43:00 -0700

    Fix page name for logged in users

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -181,6 +181,13 @@ const Header = () => {
       <div className={`${showNav ? "" : "hidden"}`}>
         <SideNav />
       </div>
+      {session && (
+        <div className="min-w-full bg-blue2">
+          <div className="flex justify-center px-1 py-1 text-xl capitalize text-olive12 dark:bg-darkBlue3 dark:text-darkOlive12">
+            {currentRoute}
+          </div>
+        </div>
+      )}
     </>
   );
 };
