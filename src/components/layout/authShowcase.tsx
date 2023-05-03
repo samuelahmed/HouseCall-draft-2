7119d98b2b732574766e07a30b4d0d85e61c30a8
samuelahmed <samueladuval@gmail.com>
2023-05-03 01:32:02 -0700

    Update auth login & register style

--- src/components/layout/authShowcase.tsx
+++ src/components/layout/authShowcase.tsx
@@ -5,22 +5,26 @@ const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
   return (
     <div className="flex max-h-5vh min-h-5vh items-center">
-      <button
-        className="mr-2 rounded-lg px-4 py-2 text-sm font-medium text-darkOlive12 hover:bg-blue10 focus:outline-none lg:py-2.5 marker:lg:px-5"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Logout" : "Login"}
-      </button>
-      <Link
-        className={
-          sessionData
-            ? "hidden"
-            : "visible mr-6 rounded-lg px-4 py-2 text-sm font-medium text-darkOlive12 hover:bg-blue10"
-        }
-        href={"/register"}
-      >
-        Register
-      </Link>
+      <div className="bg-blue12 py-1 px-1 dark:bg-darkBlue1">
+        <button
+          className="cursor-pointer bg-blue12 px-2 py-1 text-olive2 hover:outline hover:outline-2 hover:outline-blue4 active:bg-blue5 active:text-darkOlive2 dark:bg-darkBlue1"
+          onClick={sessionData ? () => signOut() : () => signIn()}
+        >
+          {sessionData ? "Logout" : "Login"}
+        </button>
+      </div>
+      <div className="bg-blue12 py-1 px-1 dark:bg-darkBlue1">
+        <Link
+          className={
+            sessionData
+              ? "hidden"
+              : "mr-2 cursor-pointer bg-blue12 px-2 py-1 text-olive2 hover:outline hover:outline-2 hover:outline-blue4 active:bg-blue5 active:text-darkOlive2 dark:bg-darkBlue1"
+          }
+          href={"/register"}
+        >
+          Register
+        </Link>
+      </div>
     </div>
   );
 };
