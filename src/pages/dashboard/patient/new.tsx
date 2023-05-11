e6cae977e0daeb76e4c6252e9eb212106e6cf673
samuelahmed <samueladuval@gmail.com>
2023-05-10 17:46:01 -0700

    Add standard button to new page

--- src/pages/dashboard/patient/new.tsx
+++ src/pages/dashboard/patient/new.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/layout/header";
 import { useRouter } from "next/router";
 import { trpc } from "@/utils/trpc";
 import NoSessionFound from "@/components/layout/noSessionFound";
+import { Button } from "@/components/ui/button";
 
 const New: NextPage = () => {
   const { data: session } = useSession();
@@ -48,33 +49,32 @@ const New: NextPage = () => {
                   <ul>
                     {readAllNewSessionsByUser
                       ?.filter((data) => {
-                        const { sessionMonth, sessionDay, sessionYear } =
-                        data;
-                      //year
-                      if (sessionYear && sessionYear < currentYear) {
-                        return false;
-                      }
-                      //month
-                      if (
-                        sessionYear &&
-                        sessionYear === currentYear &&
-                        sessionMonth &&
-                        sessionMonth < currentMonth
-                      ) {
-                        return false;
-                      }
-                      //day
-                      if (
-                        sessionYear &&
-                        sessionYear === currentYear &&
-                        sessionMonth &&
-                        sessionMonth === currentMonth &&
-                        sessionDay &&
-                        sessionDay < currentDay
-                      ) {
-                        return false;
-                      }
-                      return true;
+                        const { sessionMonth, sessionDay, sessionYear } = data;
+                        //year
+                        if (sessionYear && sessionYear < currentYear) {
+                          return false;
+                        }
+                        //month
+                        if (
+                          sessionYear &&
+                          sessionYear === currentYear &&
+                          sessionMonth &&
+                          sessionMonth < currentMonth
+                        ) {
+                          return false;
+                        }
+                        //day
+                        if (
+                          sessionYear &&
+                          sessionYear === currentYear &&
+                          sessionMonth &&
+                          sessionMonth === currentMonth &&
+                          sessionDay &&
+                          sessionDay < currentDay
+                        ) {
+                          return false;
+                        }
+                        return true;
                       })
                       ?.sort((a, b) => {
                         const aDate = new Date(
@@ -217,17 +217,15 @@ const New: NextPage = () => {
                               </div>
                             </div>
                             <div className="my-4 flex items-center justify-center">
-                              <div className="bg-blue10 py-1 px-1 dark:bg-darkBlue7">
-                                <button
-                                  type="button"
-                                  onClick={() => {
-                                    router.push(`/careSession/${data.slug}`);
-                                  }}
-                                  className="cursor-pointer bg-blue10 px-2 text-lg text-olive2 hover:outline hover:outline-2 hover:outline-blue4 active:bg-blue5 active:text-darkOlive2 dark:bg-darkBlue7"
-                                >
-                                  Details
-                                </button>
-                              </div>
+                              <Button
+                                variant="default"
+                                size="default"
+                                onClick={() => {
+                                  router.push(`/careSession/${data.slug}`);
+                                }}
+                              >
+                                Details
+                              </Button>
                             </div>
                           </li>
                         );
