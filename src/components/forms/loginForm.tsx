759c112050d83f4b5eaae6cbbd413604e93befc9
samuelahmed <samueladuval@gmail.com>
2023-05-10 17:27:54 -0700

    fix button on login form

--- src/components/forms/loginForm.tsx
+++ src/components/forms/loginForm.tsx
@@ -46,13 +46,8 @@ const LoginForm = () => {
         <div className="my-4 flex justify-center">
           {/* <div className="bg-blue10 py-1 px-1 dark:bg-darkBlue7"> */}
             <Button type="submit" variant="default" size="default">
-              Logins
+              Login
             </Button>
-            {/* <input
-              type="submit"
-              className="cursor-pointer bg-blue10 px-2 py-1 text-lg text-olive2 hover:outline hover:outline-2 hover:outline-blue4 active:bg-blue5 active:text-darkOlive2 dark:bg-darkBlue7"
-            /> */}
-          {/* </div> */}
         </div>
       </form>
     </div>
