da3e1eb066551021524668598e170fb2a021c63b
samuelahmed <samueladuval@gmail.com>
2023-05-13 23:21:01 -0700

    Prevent auto-zoom on mobile devices when using search

--- src/lib/zoomHandler.ts
+++ src/lib/zoomHandler.ts
@@ -1,11 +1,6 @@
-
-
+//Prevents zooming on double tap on mobile devices
 
 export  function getIOSInputEventHandlers() {
-//   if (isIOS()) {
-//     return {};
-//   }
-
   return {
     onTouchStart: (e: { currentTarget: { style: { fontSize: string; }; }; }) => {
       e.currentTarget.style.fontSize = "16px";
