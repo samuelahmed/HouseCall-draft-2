771f56c5e2f8e9b7d85b60b3cd9a64e531678194
samuelahmed <samueladuval@gmail.com>
2023-05-21 19:01:16 -0700

    Add start of createPaymentIntent API

--- src/server/trpc/router/stripe.ts
+++ src/server/trpc/router/stripe.ts
@@ -57,10 +57,6 @@ export const stripeRouter = router({
         stripeAccountId: z.string(),
       })
     )
-//     const loginLink = await stripe.accounts.createLoginLink(
-//   'acct_1032D82eZvKYlo2C'
-// );
-
     .mutation(async ({ input }) => {
       const { stripeAccountId } = input;
       const loginLink = await stripe.accounts.createLoginLink(
@@ -70,4 +66,61 @@ export const stripeRouter = router({
     }
     ),
     
+
+
+  //create a payment intent
+  createPaymentIntent: privateProcedure
+    
+    .input(
+      z.object({
+        line_items: z.array(z.object({
+          price: z.string(),
+          quantity: z.number(),
+        })),
+        payment_intent_data: z.object({
+          //this take a set amount (how to make it a dynamic percentage)
+          application_fee_amount: z.number(),
+          transfer_data: z.object({
+            destination: z.string(),
+          }),
+        }),
+        success_url: z.string(),
+        cancel_url: z.string(),
+
+      })
+    )
+    .mutation(async ({ input }) => {
+      const { line_items, payment_intent_data, success_url, cancel_url } = input;
+      const paymentIntent = await stripe.checkout.sessions.create({
+        mode: 'payment',
+        line_items: line_items,
+        payment_intent_data: payment_intent_data,
+        success_url: success_url,
+        cancel_url: cancel_url,
+      });
+      return paymentIntent;
+    }),
+
+
+
+
+    // const session = await stripe.checkout.sessions.create({
+    //   mode: 'payment',
+    //   line_items: [
+    //     {
+    //       price: '{{PRICE_ID}}',
+    //       quantity: 1,
+    //     },
+    //   ],
+    //   payment_intent_data: {
+    //     application_fee_amount: 123,
+    //     transfer_data: {
+    //       destination: '{{CONNECTED_ACCOUNT_ID}}',
+    //     },
+    //   },
+    //   success_url: 'https://example.com/success',
+    //   cancel_url: 'https://example.com/cancel',
+    // });
+
+
 });
